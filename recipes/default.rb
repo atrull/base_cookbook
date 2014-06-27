e3b16b3b72449f6dcb8cffcdb65b5d292eee6023
Alex Trull <alextrull@getchef.com>
2014-06-27 16:13:27 +0100

    Windows support example via 7zip

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,5 +1,12 @@
-include_recipe "sudo"
+
+case node["platform_family"]
+when "ubuntu","debian","rhel","suse"
+  include_recipe "sudo"
+  include_recipe "base::user_alextrull"
+when "windows"
+  include_recipe "7zip"
+end
+
 include_recipe "chef-client"
 include_recipe "chef-client::delete_validation"
 
-include_recipe "base::user_alextrull"
